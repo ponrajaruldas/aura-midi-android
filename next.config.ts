a2b7447b3896057cfadeea11ff6c33ae3bbaabce
ponrajaruldas <ponrajaruldas@gmail.com>
2026-05-16 11:49:34 +0530

    Add next.config.ts with static export enabled

---
+++ next.config.ts
@@ -0,0 +1,18 @@
+import type { NextConfig } from "next";
+import withPWAInit from "next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+});
+
+const nextConfig: NextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default withPWA(nextConfig);
